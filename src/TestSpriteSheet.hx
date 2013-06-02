5073dce5c11d02cd857cbb70a02617795f113fba
sonygod <sonygodx@163.com>
2013-06-02 19:27:03 +0800

    comine two demo,include camera and Joystick

--- src/TestSpriteSheet.hx
+++ src/TestSpriteSheet.hx
@@ -63,11 +63,120 @@ class TestSpriteSheet {
        
 			
 		
-			 System.root.addChild(new Entity()
-			 .add(new Joystick(100,400,pack))
-            );
+			
+			
+			
+			 trace("Loading complete!");
+
+// Add a filled background color
+        System.root.addChild(new Entity()
+        .add(new FillSprite(0x303030, System.stage.width, System.stage.height)));
+		
+		TestSpriteSheet.pack = pack;
+
+		  container= new Entity();
+		 System.root.addChild(container);
+       
+		container.add(new Sprite());
+		
+		
+		
+		//container.addChild( new Entity()
+		              //.add(new ImageSprite(pack.getTexture("bg"))));
+                       //  .add(bgwater));
+		container.addChild(new Entity().add(new YSort()));
+	  var water:AnimSprite=addAmination("bgwater", [ 0,1, 2, 3, 4, 5,6], 12, new Point(1640/4, 480), new Point(1640/4, 480), 9, 2);
+		water.sort = false;
+		water.scaleX._ = 3.8;
+		water.scaleY._ = 2.5;
+	  var bird:AnimSprite = addAmination("bird", [ 1, 2, 3, 4, 5, 6], 6, new Point(100, 500), new Point(1600, 100), 10, 1.5);		
+			bird.scaleX._ = -1;
+		addAmination("mj1", [ 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], 12, new Point(0, 470), new Point(1500, 470), 9, 1.5);
+		addAmination("weiyang", [1, 2, 3, 4, 5,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 10, new Point(100, 450), new Point(1500, 450),5,1.5);
+		addAmination("guanyu", [ 1, 2, 3, 4, 5], 10, new Point(100, 400), new Point(1600, 400), 11,1.5);
+	  var man= addAmination("mus3", [ 1, 2, 3, 4, 5, 7], 12, new Point(1500, 450), new Point(100, 450), 12, 1.5);
+man.scaleX._ = -1.5;
+	var car:AnimSprite=addAmination("car", [ 0,1, 2], 8, new Point(1500, 350), new Point(100, 450), 20,1.5);		 
+		
+	  var camera:GCamera = new GCamera(container, new Rectangle(0, 0, 1800, 470));
+	   
+	   
+	    var man2= addAmination("monster2", [ 1, 2, 3, 4, 5, 7,8,9,10,11,12,13,14,15], 8, new Point(1800, 450), new Point(1800, 450), 60, 2);
+man2.scaleX._ = -2;
+		 var font = new Font(pack, "tinyfont");
+        System.root.addChild(new Entity()
+            .add(new TextSprite(font))
+            .add(new FpsDisplay()));
+		
+			var cam:Entity=new Entity()
+                .add(camera)
+				.add(new Script());
+			System.root.addChild(cam);
+			
+			
+			
+			
+			
+	
+			camera.drag = true;
+		
+			camera.to(1500, 100, 1, 12);
 			
 		
+			
+		/*	cam.get(Script).run(new Repeat(new Sequence([
+			  new CameraMove(camera, 1500, 100, 1, 12),
+			  new CameraMove(camera,100,100,1,12)
+			
+			])));*/
+			
+			/*cam.get(Script).run(
+			new Repeat(
+			new Sequence([
+			 new CallFunction(function () {
+				if (car.x._ > 900) {
+					car.x.animateTo(0, 4);
+				car.scaleX._ = 1.5;
+				}
+				 trace("ready start"); } ),
+				
+			  new Delay(2),
+			  new CameraMove(camera, 800, 100, 1, 4),
+			  new Delay(1),
+			   new CameraMove(camera, 1500, 100, 1, 4),
+			     new Delay(3),
+				new CallFunction(function () {
+					var guanyu2:AnimSprite=addAmination("monster", [ 1, 2, 3, 4, 5], 4, new Point(1600, 400+Math.random()*200), new Point(100, 400+Math.random()*200), 20,1);
+				    guanyu2.scaleX._ = -1;
+				car.x.animateTo(1000, 5);
+				car.scaleX._ = -1.5;
+				man.scaleX._ = 1.5;
+				
+					}),
+			   new CameraMove(camera, 100, 100, 1.1, 5),
+			   new Delay(2),
+			   new CallFunction(function () { trace("finish camera!"); } ),
+			
+			])));*/
+			
+			
+			/*cam.get(Script).run(
+			
+			new Repeat(
+			new Sequence([
+			  new CallFunction(function () { trace("repeat1"); } ),
+			  new Delay(2),
+			   new CallFunction(function () { trace("repeat2"); } ),
+			   new Delay(2),
+			
+			])
+			
+			)
+			);*/
+			
+		 System.root.addChild(new Entity()
+			 .add(new Joystick(100,400,pack))
+            );
 			
     }
 	
--- src/flambe/display/Joystick.hx
+++ src/flambe/display/Joystick.hx
@@ -5,7 +5,8 @@ import flambe.input.MouseEvent;
 import flambe.input.PointerEvent;
 import flambe.math.FMath;
 import flambe.System;
-
+import flambe.display.Sprite;
+import flambe.display.ImageSprite;
 /**
  * ...
  * @author sonygod
@@ -22,6 +23,7 @@ class Joystick extends Sprite
 	private var decay:Float;
 	private var xSpeed:Float;
 	private var tension:Float;
+	private var h:Float;
 	public function new(xx:Float,yy:Float,pack:AssetPack) 
 	{
 		
@@ -31,10 +33,11 @@ class Joystick extends Sprite
 		decay = 0.5;
 		xSpeed = 0;
 		tension = 0.5;
-		sort = false;
+		//sort = false;
 		x._ = xx;
 		y._ = yy;
 		initChild(pack);
+		
 	}
 	
 	
@@ -44,21 +47,23 @@ class Joystick extends Sprite
 		
 		knob = new ImageSprite(pack.getTexture("knob"));
 	    ring = new ImageSprite(pack.getTexture("ring"));
-		knob.userName = "knob";
-		knob.sort = false;
-		ring.sort = false;
-		ring.userName = "ring";
+		shaft = new ImageSprite(pack.getTexture("shaft"));
+		//knob.sort = false;
+		//ring.sort = false;
 		knob.centerAnchor();
 		ring.centerAnchor();
-		ring.namex = "ringx";
+		shaft.setAnchor(0, shaft.getNaturalHeight() / 2);
 		
-		
-		
+		knob.userName = "knob";
+		ring.userName = "ring";
+		shaft.userName = "shaft";
 		
 		_initX = this.x._;
 		_initY = this.y._;
 		
-
+		centerAnchor();
+		
+this.alpha.animateTo(0.5,0.5);
 		
 	}
 	
@@ -66,18 +71,25 @@ class Joystick extends Sprite
 	override  public function onAdded ():Void
     {
 		
-		this.pointerMove.connect(onPointMove);
+		System.pointer.move.connect(onPointMove);
 		//var owner:Entity=cast this.owner 
 		
 		
 		var e2 = new Entity();
+		e2.name = "ring";
 		e2.add(ring);
 		var e = new Entity();
+		e.name = "knob";
 		e.add(knob);
+		var e3 = new Entity();
+		e3.name = "shaft";
+		e3.add(shaft);
 		
+		trace("nothing");
 		owner.addChild(e2);
+		owner.addChild(e3);
 	owner.addChild(e);
-	knob.namex = "knob";
+	h= shaft.getNaturalWidth();
 		knob.pointerDown.connect(onPointDown);
 		
 		System.pointer.up.connect(onUp);
@@ -85,13 +97,13 @@ class Joystick extends Sprite
 	
 	private function onPointDown(e:PointerEvent):Void {
 		_drag = true;
-		
+		this.alpha.animateTo(0.9,0.5);
 	}
 	
 	
 	private function onUp(e:PointerEvent):Void {
 		_drag = false;
-		
+		this.alpha.animateTo(0.5,0.5);
 	}
 	
 	
@@ -102,18 +114,30 @@ class Joystick extends Sprite
 		if (!_drag&&knob!=null) {
 				xSpeed = -knob.x._*tension+(xSpeed*decay);
 					knob.x._ += xSpeed;
+					shaft.alpha._ = 0;
 		}
 	}
 	
 	private function onPointMove(e:PointerEvent):Void {
 		
 		if (_drag) {
-			var angel = FMath.toDegrees(Math.atan2(System.pointer.y - _initY, System.pointer.x - _initX));
+			var angel = Math.atan2(System.pointer.y - _initY, System.pointer.x - _initX)/(Math.PI/180);
 		     
+			//trace(angel, System.pointer.y, _initY);
 		   var rotation= angel;
 			
+		   this.rotation._ = rotation;
 		   knob.rotation._ = -angel;
 		   knob.x._ = e.viewX;
+		   
+		 
+		  var h2:Float = (knob.x._ -shaft.x._) / h;
+		  //shaft.scaleX._ =h/h2;
+					shaft.alpha._ = 1;
+		   if( knob.x._ > 75 ||knob.x._ < 75){
+							knob.x._ = 75;
+						}
+						trace(knob.x);
 		  
 			}
 	}
